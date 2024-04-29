0e913a6f17697ca6a36cc8a8572d249e397013ac
watashijxxnsuka <jxxn2538@gmail.com>
2024-04-29 22:43:30 +0900

    middle 4 수정

--- Java_Homework_Study Joon/4.29/middle/middle4.java
+++ Java_Homework_Study Joon/4.29/middle/middle4.java
@@ -6,14 +6,19 @@ public class middle4 {
     public static void main(String[] args) {
 
         int[] numArr = new int[20];
+
         Random random = new Random();
-        for(int i=0;i<numArr.length;i++) numArr[i] = random.nextInt(101);
+
+        for(int i=0;i<numArr.length;i++)
+            numArr[i] = random.nextInt(101);
+
         System.out.println("<홀수번째>");
+
         for(int i=1;i<numArr.length;i+=2)
             System.out.println(i+"번째: "+numArr[i]);
-        System.out.println("<짝수번째>");
-        for(int i=numArr.length-2;i>=0;i-=2)
-            System.out.println(i+"번째: "+numArr[i]);
+         System.out.println("<짝수번째>");
+         for(int i=numArr.length-2;i>=0;i-=2)
+                 System.out.println(i+"번째: "+numArr[i]);
 
     }
 }
