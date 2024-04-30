84e03b1c2e96815a1754aa3543c1d148965bf26f
Hardy0070 <hwv1219gmail.com>
2024-04-30 14:02:26 +0900

    new file

---
+++ homework/basic.java
@@ -0,0 +1,116 @@
+package homework;
+
+public class basic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+////		1. int num = 10;을 선언하고 증감연산자를 통해 다음과 같이 출력하세요. 출력된 값은 모두 증감연산자가 포함된 상태입니다.
+//		int num = 10;
+//		
+//		System.out.println(++num);
+//		System.out.println(++num);
+//		System.out.println(num--);
+//		System.out.println(--num);
+//		System.out.println(num--);
+//		System.out.println(--num);
+	    
+
+//// 2. 관계연산자(<, >) 두 개를 사 용하여 int a = 10; int b= 20; 의 관계를 다음과 같이 출력하세요. int a
+//= 10; int b = 20;
+//
+//System.out.println(a<b); System.out.println(b<a);
+// 
+
+
+// // 3. 10 ~ 1까지 거꾸로 while문으로 출력 // while 뒤에 세미콜론 do while 때만 누르기 int a = 10;
+// 
+//while(a >= 1) { System.out.println(a--); }
+
+
+//// 4. 10 ~ 1까지 거꾸로 do ~ while문으로 출력 int a = 10;
+//do { 
+//	System.out.println(a--); } while(a >= 1);
+
+		
+
+// // 5. 10 ~ 1까지 거꾸로 for문으로 출력
+//
+//for(int a = 10; a >= 1; a--) { System.out.println(a); }
+
+		
+
+
+//// 6. 1 ~ 100까지 합을 while문으로 출력 int a = 1; int b = 0;
+// 
+//while(a <= 100) { 
+//	b += a++; } System.out.println(b);
+
+		
+// // 7. 1 ~ 100까지 합을 do ~ while문으로 출력
+//
+//		int a = 1; int b = 0;
+//
+//		do { 
+//			b += a++; } 
+//		while(a <= 100); System.out.println(b);
+
+		
+
+//// 		8. 1 ~ 100까지 합을 for문으로 출력 int b = 0;
+//
+//for(int a = 1; a <= 100; a++) { b += a; } System.out.println(b);
+
+		
+////		9. int 10개짜리 배열 생성하여 int배열에 3의 배수를 차례대로 저장하세요. 그리고 거꾸로 출력하세요.
+//		
+//		int[] intArr = new int[10];
+//		
+//		// 10 9 8 7 6 5 4 3 2 1 / 9 8 7 6 5 4 3 2 1 0
+//		for(int i = intArr.length; i > 0; i--) {
+//			intArr[i-1] = i * 3;
+//			System.out.println(intArr[i-1]);
+//		}
+		
+		
+//		10. int배열을 10개 생성해서 사용자로부터 10개의 값을 입력받고 순서대로 출력하고 총합을 구하세요.
+		
+		int[] intArr = new int[10];
+		
+		
+		
+		
+		
+		
+
+//		11. int 배열로 10개의 공간을 생성하세요. 1 ~ 10까지의 임의의 수를 저장하고 홀수만 골라서 출력하세요.
+		
+		
+		
+		
+		
+		
+
+//		12. 정수 10개를 입력받아 배열에 저장하고, 이 정수 중에서 2의 배수와 3의 배수를 출력하세요.
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
