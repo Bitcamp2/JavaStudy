be4801ee3b47d53dc6fdbeb56a2e85af150be3f8
PNT-01 <sjh981217@gmail.com>
2024-05-01 18:57:56 +0900

    "24.05.01 파일 위치 변경 및 5월 1일 Homewor_Basic 업로드"

---
+++ Homework_송주헌/0501/Rect.java
@@ -0,0 +1,27 @@
+package homework;
+
+public class Rect {
+	
+	public int width;
+	public int height;
+	
+	public Rect() {
+		
+	}
+	
+	public Rect(int width, int height) {
+		this.width = width;
+		this.height = height;
+		
+	}
+	
+	public int circumference() {
+		return 2 * (this.width + this.height);
+	
+	}
+	
+	public int area() {
+		return this.width * this.height;
+		
+	}
+}
