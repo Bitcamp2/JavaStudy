799d5a9c7a22403fdcf9e7bb977e4bdfb3e99fc1
COM\planl <bobaesj20@naver.com>
2024-05-08 17:53:46 +0900

    no message

--- hw07/Advance02.java
+++ hw07/Advance02.java
@@ -2,27 +2,25 @@ package hw07;
 
 import java.util.Scanner;
 
-public class Advance01 {
+public class Advance02 {
     public static void main(String[] args) {
 
         StringBuilder sb1 = new StringBuilder();
         StringBuilder sb2 = new StringBuilder();
 
         Scanner sc = new Scanner(System.in);
-        int index = 0;
 
         System.out.println("문자열 입력: ");
         sb1.append(sc.nextLine());
 
-        sb2.append(sb1.charAt(0));
-
         for (int i = 1; i < sb1.length(); i++) {
-            if (sb2.charAt(index) != sb1.charAt(i)) {
-                sb2.append(sb1.charAt(i));
-                index++;
+            if (sb1.charAt(i - 1) == sb1.charAt(i)) {
+                sb1.delete(i, i + 1);
+                i--;
             }
+
         }
         //delete(인덱스, 값)
-        System.out.println(sb2);
+        System.out.println(sb1);
     }
 }
